d6d4f1d7548bcbc410bfb8bc34302b72c3398efc
Dmitry Volodin <mr.molkree@gmail.com>
2020-12-05 06:10:15 +0300

    cpp and csharp day 1

---
+++ cpp/day01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+inline vector<int> read_numbers(string file_name)
+{
+    ifstream infile(file_name);
+    const istream_iterator<int> start(infile), end;
+    vector<int> numbers(start, end);
+
+    return numbers;
+}
+
+int main()
+{
+    auto numbers = read_numbers("../input/input01.txt");
+    for (size_t i = 0; i < numbers.size(); ++i)
+    {
+        for (size_t j = i + 1; j < numbers.size(); ++j)
+        {
+            if (numbers[i] + numbers[j] == 2020)
+            {
+                cout << "Part 1: " << numbers[i] * numbers[j] << endl;
+            }
+            for (size_t k = j + 1; k < numbers.size(); ++k)
+            {
+                if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                {
+                    cout << "Part 2: " << numbers[i] * numbers[j] * numbers[k];
+                }
+            }
+        }
+    }
+}

c5b0b284c9a3e5ec988ca6b97d6ee5fd3e80b9e4
Dmitry Volodin <mr.molkree@gmail.com>
2020-12-05 11:13:09 +0300

    add python day 1

---
+++ python/day01.py
@@ -0,0 +1,11 @@
+numbers = []
+with open('../input01.txt') as f:
+  for line in f:
+    numbers.append(int(line.strip('\n')))
+for i, first in enumerate(numbers):
+  for j, second in enumerate(numbers[i:]):
+    if first + second == 2020:
+      print('Part 1:', first * second)
+    for k, third in enumerate(numbers[j:]):
+      if first + second + third == 2020:
+        print('Part 2:', first * second * third)

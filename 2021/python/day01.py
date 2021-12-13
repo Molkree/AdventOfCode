613e92fba578e70ca798dd1f736adc33c9c23d8e
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-13 23:14:11 +0300

    Day 01

---
+++ 2021/python/day01.py
@@ -0,0 +1,20 @@
+from utils import get_input_path
+
+with open(get_input_path(1)) as f:
+    lines = f.readlines()
+
+    # Part 1
+    increases = 0
+    for i in range(1, len(lines)):
+        if int(lines[i]) > int(lines[i - 1]):
+            increases += 1
+    assert increases == 1475
+
+    # Part 2
+    increases = 0
+    for i in range(3, len(lines)):
+        prev_sum = int(lines[i - 3]) + int(lines[i - 2]) + int(lines[i - 1])
+        cur_sum = prev_sum - int(lines[i - 3]) + int(lines[i])
+        if prev_sum < cur_sum:
+            increases += 1
+    assert increases == 1516

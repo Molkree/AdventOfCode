42c251e4b0acc225723eb673bb5445086ddf31bf
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-04 18:56:12 +0400

    Day 04. Part 2

--- 2022/python/day04.py
+++ 2022/python/day04.py
@@ -7,8 +7,12 @@ with open(get_input_path(4)) as f:
     ]
 
 fuly_contained = 0
+overlapping = 0
 for pair in pairs:
     (start_1, end_1), (start_2, end_2) = sorted(pair, key=lambda x: (x[0], -x[1]))
-    if start_2 <= end_1 and end_2 <= end_1:
-        fuly_contained += 1
+    if start_2 <= end_1:
+        overlapping += 1
+        if end_2 <= end_1:
+            fuly_contained += 1
 assert fuly_contained == 644
+assert overlapping == 926

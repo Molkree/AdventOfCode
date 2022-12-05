ca7a2c50d700c54a06e7c4f6ae607c976d59d922
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-05 10:21:25 +0400

    Day 05. Part 2

--- 2022/python/day05.py
+++ 2022/python/day05.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 from utils import get_input_path
 
 with open(get_input_path(5)) as f:
@@ -7,15 +5,27 @@ with open(get_input_path(5)) as f:
 
 stacks_split = stacks_str.splitlines()
 stacks_count = len(stacks_split[-1].split())
-stacks: list[deque[str]] = [deque[str]() for _ in range(stacks_count)]
+stacks: list[list[str]] = [[] for _ in range(stacks_count)]
 for line in stacks_split[:-1]:
     for i in range(stacks_count):
         letter = line[i * 4 + 1]
         if letter != " ":
             stacks[i].append(letter)
+stacks = [stack[::-1] for stack in stacks]
 
-for _, count, _, from_crate, _, to_crate in map(str.split, commands.splitlines()):
-    for _ in range(int(count)):
-        stacks[int(to_crate) - 1].appendleft(stacks[int(from_crate) - 1].popleft())
-top_crates = "".join("".join(crate[0]) for crate in stacks)
+stacks_2 = [stack.copy() for stack in stacks]
+for _, count_str, _, from_stack_str, _, to_stack_str in map(
+    str.split, commands.splitlines()
+):
+    count = int(count_str)
+    from_stack = int(from_stack_str) - 1
+    to_stack = int(to_stack_str) - 1
+    for _ in range(count):
+        stacks[to_stack].append(stacks[from_stack].pop())
+    stacks_2[to_stack].extend(stacks_2[from_stack][-count:])
+    stacks_2[from_stack] = stacks_2[from_stack][:-count]
+
+top_crates = "".join("".join(crate[-1]) for crate in stacks)
 assert top_crates == "JCMHLVGMG"
+top_crates_2 = "".join("".join(crate[-1]) for crate in stacks_2)
+assert top_crates_2 == "LVMRWSSPZ"

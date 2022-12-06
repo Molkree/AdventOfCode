28fe6be5135652c992b3090e3944870c380c34fb
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-06 09:37:50 +0400

    Day 06. Part 2
    Inefficient scanning again but it's fast enough

--- 2022/python/day06.py
+++ 2022/python/day06.py
@@ -2,10 +2,17 @@ from utils import get_input_path
 
 with open(get_input_path(6)) as f:
     datastream = f.readline().rstrip()
-start_marker_len = 4
-for i in range(len(datastream) - start_marker_len + 1):
-    start_marker = datastream[i : i + start_marker_len]
-    if len(set(start_marker)) == start_marker_len:
-        start_marker_pos = i + start_marker_len
-        assert start_marker_pos == 1531
+start_packet_len = 4
+start_message_len = 14
+start_packet_pos = 0
+for i in range(len(datastream) - start_packet_len + 1):
+    start_packet = datastream[i : i + start_packet_len]
+    if not start_packet_pos and len(set(start_packet)) == start_packet_len:
+        start_packet_pos = i + start_packet_len
+        assert start_packet_pos == 1531
+    # no index error cause we are guaranteed to have message marker after packet marker
+    start_message = datastream[i : i + start_message_len]
+    if len(set(start_message)) == start_message_len:
+        start_message_pos = i + start_message_len
+        assert start_message_pos == 2518
         break

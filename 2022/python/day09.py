92f128db9595c6cb2334454108bdfb7aa07ddffa
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-09 20:36:16 +0400

    Day 09. Part 2
    This took me too much time...

--- 2022/python/day09.py
+++ 2022/python/day09.py
@@ -3,29 +3,43 @@ from utils import get_input_path
 with open(get_input_path(9)) as f:
     moves = f.read().splitlines()
 
-head_pos = 0, 0
-tail_pos = 0, 0
-visited: set[tuple[int, int]] = {tail_pos}
+
+tails_count = 9
+knots: list[tuple[int, int]] = [(0, 0) for _ in range(tails_count + 1)]
+visited: list[set[tuple[int, int]]] = [{(0, 0)} for _ in range(tails_count)]
+
+
+def shift(prev_coord: int, next_coord: int) -> int:
+    if prev_coord > next_coord:
+        return 1
+    if prev_coord < next_coord:
+        return -1
+    return 0
+
+
+def move_tails(
+    tails_count: int, knots: list[tuple[int, int]], visited: list[set[tuple[int, int]]]
+) -> None:
+    for i in range(1, tails_count + 1):
+        (x1, y1), (x2, y2) = knots[i - 1], knots[i]
+        if abs(x1 - x2) < 2 and abs(y1 - y2) < 2:
+            break
+        knots[i] = x2 + shift(x1, x2), y2 + shift(y1, y2)
+        visited[i - 1].add(knots[i])
+
+
 for move in moves:
     direction, distance = move[0], int(move[2:])
-    if direction == "U":
-        head_pos = head_pos[0], head_pos[1] + distance
-        for y in range(tail_pos[1] + 1, head_pos[1]):
-            tail_pos = head_pos[0], y
-            visited.add(tail_pos)
-    elif direction == "D":
-        head_pos = head_pos[0], head_pos[1] - distance
-        for y in range(tail_pos[1] - 1, head_pos[1], -1):
-            tail_pos = head_pos[0], y
-            visited.add(tail_pos)
-    elif direction == "R":
-        head_pos = head_pos[0] + distance, head_pos[1]
-        for x in range(tail_pos[0] + 1, head_pos[0]):
-            tail_pos = x, head_pos[1]
-            visited.add(tail_pos)
-    elif direction == "L":
-        head_pos = head_pos[0] - distance, head_pos[1]
-        for x in range(tail_pos[0] - 1, head_pos[0], -1):
-            tail_pos = x, head_pos[1]
-            visited.add(tail_pos)
-assert len(visited) == 6357
+    for _ in range(distance):
+        if direction == "U":
+            knots[0] = knots[0][0], knots[0][1] + 1
+        if direction == "D":
+            knots[0] = knots[0][0], knots[0][1] - 1
+        if direction == "R":
+            knots[0] = knots[0][0] + 1, knots[0][1]
+        if direction == "L":
+            knots[0] = knots[0][0] - 1, knots[0][1]
+        move_tails(tails_count, knots, visited)
+
+assert len(visited[0]) == 6357
+assert len(visited[-1]) == 2627

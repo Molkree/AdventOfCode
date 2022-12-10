2033a520999a3e4894d5e46498d1350e9d6a8322
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-10 22:51:30 +0400

    Day 10. Part 2

--- 2022/python/day10.py
+++ 2022/python/day10.py
@@ -1,3 +1,5 @@
+import inspect
+
 from utils import get_input_path
 
 with open(get_input_path(10)) as f:
@@ -14,3 +16,24 @@ signal_strengths: list[int] = []
 for i in range(19, 221, 40):
     signal_strengths.append(history[i] * (i + 1))
 assert sum(signal_strengths) == 13220
+
+lines: list[str] = []
+for row in range(6):
+    line: list[str] = []
+    for col in range(40):
+        pixel = row * 40 + col
+        if history[pixel] - 1 <= col <= history[pixel] + 1:
+            line.append("#")
+        else:
+            line.append(".")
+    lines.append("".join(line))
+answer = inspect.cleandoc(  # RUAKHBEK
+    """
+###..#..#..##..#..#.#..#.###..####.#..#.
+#..#.#..#.#..#.#.#..#..#.#..#.#....#.#..
+#..#.#..#.#..#.##...####.###..###..##...
+###..#..#.####.#.#..#..#.#..#.#....#.#..
+#.#..#..#.#..#.#.#..#..#.#..#.#....#.#..
+#..#..##..#..#.#..#.#..#.###..####.#..#."""
+)
+assert "\n".join(lines) == answer

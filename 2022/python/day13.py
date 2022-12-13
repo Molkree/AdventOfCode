349e9b82e6ac52a0b4346b4ae951a1c18b69ba86
Dmitry Volodin <mr.molkree@gmail.com>
2022-12-13 20:53:39 +0400

    Day 13. Part 1

---
+++ 2022/python/day13.py
@@ -0,0 +1,67 @@
+from enum import Enum
+from typing import Union
+
+from utils import get_input_path
+
+with open(get_input_path(13)) as f:
+    str_pairs = map(str.splitlines, f.read().split("\n\n"))
+Packet = list[Union[int, "Packet"]]
+
+
+def parse_packet(str_packet: str) -> Packet:
+    packet = Packet()
+    stack = [packet]
+    for index, char in enumerate(str_packet[1:]):
+        current = stack[-1]
+        if char == "]":
+            stack.pop()
+        elif char.isdigit():
+            if str_packet[index].isdigit():
+                current[-1] = current[-1] * 10 + int(char)  # type: ignore # we know that last value is int
+            else:
+                current.append(int(char))
+        elif char == "[":
+            current.append(Packet())
+            stack.append(current[-1])  # type: ignore # we know that current is a list
+    return packet
+
+
+class PacketOrder(Enum):
+    LEFT = 1
+    RIGHT = 2
+    EQUAL = 3
+
+
+def get_packet_order(left: Packet, right: Packet) -> PacketOrder:
+    for index, left_value in enumerate(left):
+        if index >= len(right):
+            return PacketOrder.RIGHT
+        right_value = right[index]
+        if isinstance(left_value, int) and isinstance(right_value, int):
+            if left_value < right_value:
+                return PacketOrder.LEFT
+            elif left_value > right_value:
+                return PacketOrder.RIGHT
+        elif isinstance(left_value, list) and isinstance(right_value, list):
+            order = get_packet_order(left_value, right_value)
+            if order != PacketOrder.EQUAL:
+                return order
+        else:
+            if isinstance(left_value, int):
+                # both pyright and mypy can't figure out that right_value is a list
+                order = get_packet_order([left_value], right_value)  # type: ignore
+            else:
+                order = get_packet_order(left_value, [right_value])
+            if order != PacketOrder.EQUAL:
+                return order
+    if len(left) < len(right):
+        return PacketOrder.LEFT
+    return PacketOrder.EQUAL
+
+
+good_pairs: list[int] = []
+for index, pair in enumerate(str_pairs):
+    left, right = map(parse_packet, pair)
+    if get_packet_order(left, right) != PacketOrder.RIGHT:
+        good_pairs.append(index + 1)
+assert sum(good_pairs) == 5208

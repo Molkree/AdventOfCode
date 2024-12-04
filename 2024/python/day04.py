7f5ef913c0578f552c9b82ee5275df862f11746a
Dmitry Volodin <mr.molkree@gmail.com>
2024-12-05 02:09:10 +0400

    feat(2024): day 4, part 2

--- 2024/python/day04.py
+++ 2024/python/day04.py
@@ -51,26 +51,69 @@ def count_all_words(grid: Sequence[str]) -> int:
     return count
 
 
+def check_x_mas(grid: Sequence[str], row: int, col: int) -> bool:
+    return grid[row][col] == "A" and (
+        # M.M
+        # .A.
+        # S.S
+        grid[row - 1][col - 1] == "M"
+        and grid[row - 1][col + 1] == "M"
+        and grid[row + 1][col - 1] == "S"
+        and grid[row + 1][col + 1] == "S"
+        # S.S
+        # .A.
+        # M.M
+        or grid[row - 1][col - 1] == "S"
+        and grid[row - 1][col + 1] == "S"
+        and grid[row + 1][col - 1] == "M"
+        and grid[row + 1][col + 1] == "M"
+        # M.S
+        # .A.
+        # M.S
+        or grid[row - 1][col - 1] == "M"
+        and grid[row - 1][col + 1] == "S"
+        and grid[row + 1][col - 1] == "M"
+        and grid[row + 1][col + 1] == "S"
+        # S.M
+        # .A.
+        # S.M
+        or grid[row - 1][col - 1] == "S"
+        and grid[row - 1][col + 1] == "M"
+        and grid[row + 1][col - 1] == "S"
+        and grid[row + 1][col + 1] == "M"
+    )
+
+
+def count_x_mases(grid: Sequence[str]) -> int:
+    count = 0
+    for row in range(1, len(grid) - 1):
+        for col in range(1, len(grid[row]) - 1):
+            count += int(check_x_mas(grid, row, col))
+    return count
+
+
 example_1 = """..X...
 .SAMX.
 .A..A.
 XMAS.S
-.X...."""
-assert count_all_words(example_1.splitlines()) == 4
+.X....""".splitlines()
+assert count_all_words(example_1) == 4
 
-example_2 = """....XXMAS.
-.SAMXMS...
-...S..A...
-..A.A.MS.X
-XMASAMX.MM
-X.....XA.A
-S.S.S.S.SS
-.A.A.A.A.A
-..M.M.M.MM
-.X.X.XMASX"""
-assert count_all_words(example_2.splitlines()) == 18
+example_2 = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".splitlines()
+assert count_all_words(example_2) == 18
+assert count_x_mases(example_2) == 9
 
 with open(get_input_path(4)) as f:
-    input_str = f.read()
+    input = f.read().splitlines()
 
-assert count_all_words(input_str.splitlines()) == 2583
+assert count_all_words(input) == 2583
+assert count_x_mases(input) == 1978

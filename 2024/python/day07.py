9f3af3ed5d8fa23f566dd04e78b07dd3a1cf8938
Dmitry Volodin <mr.molkree@gmail.com>
2024-12-29 17:24:13 +0400

    feat(2024): day 7, part 2
    ~14s with brute force

--- 2024/python/day07.py
+++ 2024/python/day07.py
@@ -1,4 +1,5 @@
-from collections.abc import Sequence
+from collections.abc import Callable, Iterable, Sequence
+from datetime import UTC, datetime
 from itertools import product
 from operator import add, mul
 
@@ -13,8 +14,9 @@ def parse_input(str_equations: Sequence[str]) -> list[tuple[int, list[int]]]:
     return equations
 
 
-def check_equation(test_value: int, numbers: list[int]) -> bool:
-    operations = [add, mul]
+def check_equation(
+    operations: Iterable[Callable[[int, int], int]], test_value: int, numbers: list[int]
+) -> bool:
     combinations = product(operations, repeat=len(numbers) - 1)
 
     for combination in combinations:
@@ -28,13 +30,28 @@ def check_equation(test_value: int, numbers: list[int]) -> bool:
     return False
 
 
-def solve_part_1(input: Sequence[str]) -> int:
+def solve_part(
+    input: Sequence[str], operations: Iterable[Callable[[int, int], int]]
+) -> int:
     equations = parse_input(input)
     total_calibration_result = 0
     for test_value, numbers in equations:
-        if check_equation(test_value, numbers):
+        if check_equation(operations, test_value, numbers):
             total_calibration_result += test_value
     return total_calibration_result
+
+
+def solve_part_1(input: Sequence[str]) -> int:
+    operations = [add, mul]
+    return solve_part(input, operations)
+
+
+def solve_part_2(input: Sequence[str]) -> int:
+    def concatenation(a: int, b: int) -> int:
+        return int(str(a) + str(b))
+
+    operations: list[Callable[[int, int], int]] = [add, mul, concatenation]
+    return solve_part(input, operations)
 
 
 example = """190: 10 19
@@ -47,7 +64,12 @@ example = """190: 10 19
 21037: 9 7 18 13
 292: 11 6 16 20"""
 assert solve_part_1(example.splitlines()) == 3749
+assert solve_part_2(example.splitlines()) == 11387
 
 with open(get_input_path(7)) as f:
     input = f.readlines()
 assert solve_part_1(input) == 975671981569
+timer_start = datetime.now(UTC)
+assert solve_part_2(input) == 223472064194845
+timer_end = datetime.now(UTC)
+print("Part 2 took:", timer_end - timer_start)
